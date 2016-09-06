3cfa51f1837ee2fc2858276f989d4bf0621f9d4b
ross <meatypocket@gmail.com>
2016-09-06 17:36:55 +0100

    remove django URL Patterns

--- photologue/tests/test_urls.py
+++ photologue/tests/test_urls.py
@@ -1,15 +1,14 @@
 from django.conf.urls import *
 from ..sitemaps import GallerySitemap, PhotoSitemap
 
-urlpatterns = patterns('',
-                       (r'^ptests/', include('photologue.urls', namespace='photologue')),
-                       )
+urlpatterns = [
+    (r'^ptests/', include('photologue.urls', namespace='photologue')),
+]
 
 sitemaps = {'photologue_galleries': GallerySitemap,
             'photologue_photos': PhotoSitemap,
             }
 
-urlpatterns += patterns('',
-                        (r'^sitemap.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps':
-                                                                                     sitemaps})
-                        )
+urlpatterns += [
+    (r'^sitemap.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps})
+]
